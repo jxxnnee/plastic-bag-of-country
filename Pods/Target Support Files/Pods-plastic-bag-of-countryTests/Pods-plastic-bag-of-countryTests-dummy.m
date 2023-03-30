7bca73208c3885b6156f077ccbc8168a76ec042e
jxxnnee <q_alz@naver.com>
2023-03-30 18:44:46 +0900

    CocoaPods 추가

---
+++ Pods/Target Support Files/Pods-plastic-bag-of-countryTests/Pods-plastic-bag-of-countryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_plastic_bag_of_countryTests : NSObject
+@end
+@implementation PodsDummy_Pods_plastic_bag_of_countryTests
+@end
---
+++ Pods/Target Support Files/Pods-plastic-bag-of-countryUITests/Pods-plastic-bag-of-countryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_plastic_bag_of_countryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_plastic_bag_of_countryUITests
+@end
